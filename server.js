8ccc496feaa817e8ed6d165b0dbf326112c69cfc
U-nitsor-MSI\nitsor <nitsor@nitsor-MSI.(none)>
2014-08-10 22:46:53 -0700

    Created index page.

---
+++ server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+	
+	fs.readFile('index.html', function(error, data) {
+		res.writeHead(200, {'Content-Type':'text/html'});
+		res.end(data);
+	});
+}).listen(1337, '127.0.0.1');
+console.log('Web server started.');
